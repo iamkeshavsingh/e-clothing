cfe8acf6e84289f88d0c1e87b0c299f338f0eaf5
Keshav Singh Yadav <keshavsinghyadav@Keshavs-MacBook-Pro.local>
2020-07-12 04:26:59 +0530

    tranformation into small components

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+import Directory from '../../components/directory/directory';
+
+const HomePage = (props) => {
+
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    );
+};
+
+export default HomePage;
