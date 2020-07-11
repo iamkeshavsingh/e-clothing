696addabd0a9f00ad726035919553dc92ebb5ea7
Keshav Singh Yadav <keshavsinghyadav@Keshavs-MacBook-Pro.local>
2020-07-12 05:26:33 +0530

    withRouter implemented in menu-items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage';
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
